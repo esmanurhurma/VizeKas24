1524d362abfc15ed8f16b06d63dc3698f33e8fe2
Esmanur Hurma <esmanurhurma@outlook.com>
2023-11-24 16:53:34 +0300

    Özellik 2 tüm testleri ile tamamlandı.

--- 190401002/Vize_Q2_190401002_esmanur_hurma.R
+++ 190401002/Vize_Q2_190401002_esmanur_hurma.R
@@ -24,3 +24,24 @@ spotify_search_artist <- function(artist_name) {
   # HTTP Status Code
   status_code <- httr::status_code(response)
   
+  # JSON Parse
+  json_data <- jsonlite::fromJSON(content(response, "text"))
+  
+  # Artist Bilgilerini Çıkartma
+  artists <- json_data$artists$items
+  artist_df <- data.frame(artist = sapply(artists, function(x) x$name),
+                          id = sapply(artists, function(x) x$id),
+                          stringsAsFactors = FALSE)
+  
+  # Çıktı Oluşturma
+  output <- list(
+    status_code = status_code,
+    search_results = artist_df
+  )
+  
+  return(output)
+}
+
+# Kullanım Örneği
+result <- spotify_search_artist("The Doors")
+print(result)
--- 190401002/Vize_Q2_tests_190401002_esmanur_hurma.R
+++ 190401002/Vize_Q2_tests_190401002_esmanur_hurma.R
@@ -15,6 +15,60 @@ test_that("Spotify API Fonksiyonu Testleri", {
     expect_is(spotify_search_artist, "function", "Test 2.2 geçemedi: spotify_search_artist değişkeninin tipi 'function' olmalı.")
   })
   
+  # Test 2.3
+  test_that("spotify_search_artist() herhangi bir artist ismi ile çağrıldığında döndürdüğü çıktı bir liste olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_is(result, "list", "Test 2.3 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü çıktı bir liste olmalı.")
+  })
+  
+  # Test 2.4
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin iki elementi olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_length(result, 2, "Test 2.4 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin iki elementi olmalı.")
+  })
+  
+  # Test 2.5
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin ismi status_code olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_identical(names(result)[1], "status_code", "Test 2.5 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin ismi status_code olmalı.")
+  })
+  
+  # Test 2.6
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin class’ı numeric olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_is(result$status_code, "numeric", "Test 2.6 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin ilk elementinin class'ı numeric olmalı.")
+  })
+  
+  # Test 2.7
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin status_code adlı elementinin değeri 200’e eşit olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_equal(result$status_code, 200, "Test 2.7 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin status_code adlı elementinin değeri 200’e eşit olmalı.")
+  })
+  
+  # Test 2.8
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin ismi search_results olmalı.", {
+    result <- spotify_search_artist("The Doors")
+    expect_identical(names(result)[2], "search_results", "Test 2.8 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin ismi search_results olmalı.")
+  })
+  
+  # Test 2.9
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin class’ı data.frame olmalı.", {
+    result <- spotify_search_artist("Random Artist")
+    expect_is(result$search_results, "data.frame", "Test 2.9 geçemedi: spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin class'ı data.frame olmalı.")
+  })
+  
+  # Test 2.10
+  test_that("spotify_search_artist() çağrıldığında döndürdüğü listenin ikinci elementinin iki sütun barındırmalı.", {
+    result
+    
+    # Test 2.11
+    test_that("spotify_search_artist(the doors) çağrıldığında döndürdüğü listenin ikinci elementinin birinci satırının “id” adlı sütunu “22WZ7M8sxp5THdruNY3gXt” olmalı", {
+      result <- spotify_search_artist("Random Artist")
+      
+      
+      
+      
+  
   
 
 
